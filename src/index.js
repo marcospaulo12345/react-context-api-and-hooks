3b29d4404cf97cdef778a5ae28b28384b40b2055
marcospaulo12345 <marcospaulo.mpf34@gmail.com>
2023-01-03 15:03:22 -0300

    criado o custom hook e o context

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
+import { CounterContextProvider } from './contexts/CounterContext';
 
 import './styles/global-styles.css';
 
@@ -10,6 +11,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Home />
+    <CounterContextProvider>
+      <Home />
+    </CounterContextProvider>
   </React.StrictMode>,
 );
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,4 +1,10 @@
+// eslint-disable-next-line prettier/prettier
+import { useCounterContext } from "../../contexts/CounterContext";
+
 export const Home = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [state, dispatch] = useCounterContext();
+
   return (
     <div>
       <h1>Oi</h1>
